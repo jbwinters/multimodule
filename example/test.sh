affd3470f3c3a4bd2703967c33094a47f7ae32d5
Josh Winters <josh@idealspot.com>
2016-09-04 23:04:10 -0500

    Add scripts to run example

---
+++ example/test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Run run.sh separately
+
+echo "* v0 (multiapi/v0)"
+curl localhost:5000/v0
+echo "\n* v1 (multiapi/v1)"
+curl localhost:5000/v1
+echo "\n* v1.1 (multiapi/v1_1)"
+curl localhost:5000/v1.1
+echo "\n* v2 (multiapi/v2)"
+curl localhost:5000/v2
+echo "\n* v3 (non-existing)"
+curl localhost:5000/v3
